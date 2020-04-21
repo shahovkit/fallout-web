58b11d0c2b4b9ad0d8c86698b2a6b5731f95b3b5
shahovkit@gmail.com <Littlecutegitlab1>
2020-04-21 11:03:40 +0300

    Restructurize project

--- res/Graphics.js
+++ res/Graphics.js
@@ -1,7 +1,27 @@
 import {Units} from "./Units.js";
+import {Conversion} from "./Conversion.js";
+import {GameMap} from "./GameMap.js";
 
 export class Graphics
 {
+
+    static instance;
+
+    static getInstance(){
+        if( typeof(this.instance) === "undefined"){
+            this.instance = new this();
+        }
+        return this.instance
+    }
+
+    constructor(){
+        if( typeof(this.instance) !== "undefined"){
+            throw new Error("Object already exist, use getInstance() instead new")
+        }
+        this.instance = this;
+
+    }
+
     static initGame(){
 
     }
@@ -19,5 +39,12 @@ export class Graphics
         return coords;
     }
 
-
+    static drawPointer(){
+        if (Pointer.positionByHex.y >= 0 && Pointer.positionByHex.x >= 0) {
+            let oddr = Conversion.pixel2Offset(Pointer.positionByHex);
+            if (oddr.q < GameMap.getInstance().getSize().q && oddr.r < GameMap.getInstance().getSize().r) {
+                graphics.strokePoints(Graphics.hex_corners(Pointer.positionByHex, Constants.sizeHex, 6), true);
+            }
+        }
+    }
 }
